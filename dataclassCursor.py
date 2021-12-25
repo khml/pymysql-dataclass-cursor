1551e1eafd2bca3547c6f18a260b4e3da8a3c9bd
khmlpy <43922054+khml@users.noreply.github.com>
2021-12-25 21:08:43 +0900

    DataclassCursor

---
+++ dataclassCursor.py
@@ -0,0 +1,49 @@
+import dataclasses
+
+import pymysql
+
+
+def _get_attrs(self):
+    return self.__dict__.keys()
+
+
+def _as_dict(self):
+    return dataclasses.asdict(self)
+
+
+CLASS_NAME = 'Record'
+
+
+def _to_dataclass(data: dict, frozen: bool = False):
+    attrs = [(key, type(val)) for key, val in data.items()]
+
+    data_cls = dataclasses.make_dataclass(
+        CLASS_NAME,
+        attrs,
+        namespace=dict(attrs=_get_attrs, as_dict=_as_dict),
+        frozen=frozen
+    )
+
+    return data_cls(**data)
+
+
+class _DataclassCursorMixin(pymysql.cursors.DictCursorMixin):
+    def _conv_row(self, row):
+        if row is None:
+            return None
+        return _to_dataclass(dict(zip(self._fields, row)), frozen=False)
+
+
+class DataclassCursor(_DataclassCursorMixin, pymysql.cursors.Cursor):
+    """DataclassCursor"""
+
+
+class _FrozenDataclassCursorMixin(pymysql.cursors.DictCursorMixin):
+    def _conv_row(self, row):
+        if row is None:
+            return None
+        return _to_dataclass(dict(zip(self._fields, row)), frozen=True)
+
+
+class FrozenDataclassCursor(_FrozenDataclassCursorMixin, pymysql.cursors.Cursor):
+    """DataclassCursor"""
